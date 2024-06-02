3a060ee4b2ce41bd44dc2ff954f50998b813cbd3
Mark Hawkins <mark.hawkins@gmail.com>
2024-06-02 17:04:56 +0100

    New project called headlines and requirements.txt

---
+++ headlines/headlines.py
@@ -0,0 +1,23 @@
+import feedparser
+
+from flask import Flask
+
+app = Flask (__name__)
+
+RSS_FEED = "https://omgubuntu.co.uk/feed"
+
+@app.route("/")
+def get_news():
+  feed = feedparser.parse(RSS_FEED)
+  first_article = feed['entries'][0]
+  return """<html>
+    <body>
+      <h1>Headlines</h1>
+      <b>{0}</b> <br />
+      <i>{1}</i> <br />
+      <p>{2}</p> <br />
+    </body>
+  </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
